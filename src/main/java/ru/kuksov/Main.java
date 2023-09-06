5ebde8c23ca242b65c841bbe32b75aa0fec98c9a
Alex286756 <ai286756@gmail.com>
2023-09-06 22:24:18 +0600

    change 'Integer' on 'Object'

--- src/main/java/ru/kuksov/Main.java
+++ src/main/java/ru/kuksov/Main.java
@@ -1,7 +1,47 @@
 package ru.kuksov;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student1 = new Student("Alex");
+        ArrayList<Object> assessment1 = new ArrayList<>();
+        assessment1.add('5');
+        assessment1.add('4');
+        assessment1.add(3);
+        assessment1.add(4);
+        Student student2 = new Student("Peter", assessment1);
+        ArrayList<Object> assessment2 = new ArrayList<>();
+        assessment2.add('5');
+        assessment2.add('4');
+        assessment2.add(3);
+        assessment2.add(4);
+        Student student3 = new Student("Alexandr", assessment2);
+        student3.setName("Alex");
+        System.out.println("Студенты 1 и 2 равны? Ответ: " + student2.equals(student1));
+        System.out.println("Студенты 2 и 3 равны? Ответ: " + student2.equals(student3));
+        System.out.println("Студенты 1 и 3 равны? Ответ: " + student3.equals(student1));
+        student1.addAssessment(5);
+        student1.addAssessment(4);
+        student1.addAssessment(3);
+        student1.addAssessment(4);
+        System.out.println("Студенты 1 и 3 равны? Ответ: " + student3.equals(student1));
+        System.out.println("Студент 1: " + student1);
+        System.out.println("Студент 1: " + student2);
+        System.out.println("Студент 1: " + student3);
+        System.out.println();
+        student1.delAssessment(4);
+        System.out.println("Студент 1: " + student1);
+        System.out.println("Студент 3: " + student3);
+        System.out.println("Студенты 1 и 3 равны? Ответ: " + student3.equals(student1));
+        student3.delAssessment(4);
+        System.out.println("Студент 1: " + student1);
+        System.out.println("Студент 3: " + student3);
+        System.out.println("Студенты 1 и 3 равны? Ответ: " + student3.equals(student1));
+        student3.delAssessment(4);
+        System.out.println("Студент 1: " + student1);
+        System.out.println("Студент 3: " + student3);
+        System.out.println("Студенты 1 и 3 равны? Ответ: " + student3.equals(student1));
+
     }
 }
--- src/main/java/ru/kuksov/Student.java
+++ src/main/java/ru/kuksov/Student.java
@@ -1,33 +1,35 @@
 package ru.kuksov;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Objects;
 
 public class Student {
 
     private String name;
-    private ArrayList<Integer> assessments;
+    private ArrayList<Object> assessments;
 
     public Student(String name) {
         this.name = name;
         assessments = new ArrayList<>();
     }
 
-    public Student(String name, ArrayList<Integer> assessments) {
-        this.name = name;
-        this.assessments = assessments;
+    public Student(Object name, Object assessments) {
+        if (name instanceof String)
+            this.name = (String) name;
+        if (assessments instanceof ArrayList<?>)
+            this.assessments = (ArrayList<Object>) assessments;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(Object name) {
+        if (name instanceof String)
+            this.name = (String) name;
     }
 
-    public void addAssessment(Integer newAssessment) {
+    public void addAssessment(Object newAssessment) {
         assessments.add(newAssessment);
     }
 
@@ -35,7 +37,7 @@ public class Student {
         assessments.remove(oldAssessment);
     }
 
-    public ArrayList<Integer> getAssessments() {
+    public ArrayList<Object> getAssessments() {
         return assessments;
     }
 
@@ -44,16 +46,16 @@ public class Student {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        return Objects.equals(name, student.name) && Objects.equals(assessments, student.assessments);
+        return Objects.equals(getName(), student.getName()) && Objects.equals(getAssessments(), student.getAssessments());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, assessments);
+        return Objects.hash(getName(), getAssessments());
     }
 
     @Override
     public String toString() {
-        return name + ": [" + assessments + ']';
+        return getName() + ": " + getAssessments();
     }
 }
